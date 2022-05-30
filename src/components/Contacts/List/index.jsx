11ce66d6f702ee1941d86ad15ea5fdf5d55bc0d6
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-05-30 04:19:36 +0300

    input onchange

--- src/components/Contacts/List/index.jsx
+++ src/components/Contacts/List/index.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import {useState} from "react";
 
 function List({ contacts }) {
+  const [filterText, setFilterText] = useState("");
+
   return (
     <div>
-      <input placeholder="Filter contact" />
+      <input placeholder="Filter contact" value={filterText} onChange={(e) => setFilterText(e.target.value)} />
       <ul>
         {contacts.map((contact, i) => (
           <li key={i}>{contact.fullname}</li>
