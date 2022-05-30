131ebb91c339a83622964fe0fff721f27f2d4acc
emre15900 <79264023+emre15900@users.noreply.github.com>
2022-05-30 03:50:38 +0300

    prettier

--- src/components/Contacts/Form/index.jsx
+++ src/components/Contacts/Form/index.jsx
@@ -1,30 +1,37 @@
 import { useState } from "react";
 
-function Form({addContact, contacts}) {
-
+function Form({ addContact, contacts }) {
   const [form, setForm] = useState({ fullname: "", phone_number: "" });
   const onChangeInput = (e) => {
-    setForm({...form, [e.target.name]: e.target.value})
-  }
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    if(form.fullname === "" || form.phone_number === ""){
+    if (form.fullname === "" || form.phone_number === "") {
       return false;
     }
-    
+
     addContact([...contacts, form]);
-    console.log(form)
-  }
+    console.log(form);
+  };
 
   return (
     <form onSubmit={onSubmit}>
       <div>
-        <input name="fullname" placeholder="Full Name" onChange={onChangeInput} />
+        <input
+          name="fullname"
+          placeholder="Full Name"
+          onChange={onChangeInput}
+        />
       </div>
       <div>
-        <input name="phone_number" placeholder="Phone Number" onChange={onChangeInput} />
+        <input
+          name="phone_number"
+          placeholder="Phone Number"
+          onChange={onChangeInput}
+        />
       </div>
       <div>
         <button>Add</button>
--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,21 +1,21 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
-import Form from './Form'
-import List from './List'
+import Form from "./Form";
+import List from "./List";
 
 function Contacts() {
-  const [contacts, setContacts] = useState([])
-  
-  useEffect(()=>{
-    console.log(contacts)
-  },[contacts])
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
 
   return (
     <div>
-        <List />
-        <Form addContact={setContacts} contacts={contacts} />
+      <List />
+      <Form addContact={setContacts} contacts={contacts} />
     </div>
-  )
+  );
 }
 
-export default Contacts
+export default Contacts;
